5f321df04296ad227afaaaf9ea9d3ad2748c5c3b
Jhemarr Anderson <jhemarranderson@Jhemarrs-MacBook-Pro.local>
2017-07-20 17:04:46 -0400

    AJAX working

--- public/js/pixabay.js
+++ public/js/pixabay.js
@@ -1,35 +1,72 @@
-// On Click function for each gif button that pushes that value into the queryURL variable
-$("#search").click(function(){
-   event.preventDefault();
-  console.log("Search button was clicked!!!!");
-  // empties the div with id food-gifs so that only those of the button clicked are shown
-  $("#city-info").empty();
+//Pixelbay API
 
-// stored the data-name in the variable gifClicked
-var cityInput = $(this).attr("data-name");
-// console.log("Gif Button was clicked! I have a data-name of: " + gifClicked);
-  // URL that gets called in the ajax function that is requesting info from the Giphy API
-  var queryURL = 
-  "https://pixabay.com/api/?key=5926841-97276293940f3f4726623eeab&q=" + cityInput;
-  // console.log(queryURL);
-  // ajax function that requests information from the Giphy API
+function getPixelbay (){
+  event.preventDefault;
+// stores data-name in variable city input after click
+  var cityInput = $(this).attr("data-name");
+
+  var queryURL = "https://pixabay.com/api/?key=5926841-97276293940f3f4726623eeab&q=" + cityInput;
+  console.log(queryURL);
+
   $.ajax({
     url: queryURL,
-    method: "GET"
+    method: "GET",
+    cors: true
   });
-// once response is given from the giphy API do all this stuff: 
-  .done(function(response) {
-    console.log(response);
-    // get the data from response and store it in variable gif
-    var cityImage = response;
-      // console.log(gif);
-// loop through the gif reponse data
-      for (i = 0; i < cityImage.length; i++){
-        // create p tag
-        var p = $("<p>");
-        //add class of rating to p tags
-        p.addClass("rating");
-        // add text to p tag of Rating: and the rating from the gif responses
-        // p.text("Rating: " + gif[i].rating);
-        // store img tag in variable gifImg
-        var cityImg = $("<img>");
+}
+
+// starts on click function
+
+$("#search").on("click", function(event){
+  event.preventDefault();
+  console.log("CLICK WORKS")
+  $("#city-info").empty();
+
+  getPixelbay();
+
+});
+
+
+
+
+// // On Click function for each gif button that pushes that value into the queryURL variable
+// $("#search").click(function(){
+//    event.preventDefault();
+//   console.log("Search button was clicked!!!!");
+//   // empties the div with id food-gifs so that only those of the button clicked are shown
+//   $("#city-info").empty();
+
+// // stored the data-name in the variable gifClicked
+// var cityInput = $(this).attr("data-name");
+// // console.log("Gif Button was clicked! I have a data-name of: " + gifClicked);
+//   // URL that gets called in the ajax function that is requesting info from the Giphy API
+//   var queryURL = 
+//   "https://pixabay.com/api/?key=5926841-97276293940f3f4726623eeab&q=" + cityInput;
+//   console.log(queryURL);
+//   // ajax function that requests information from the Giphy API
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   });
+// // once response is given from the giphy API do all this stuff: 
+//   .done(function(response) {
+//         console.log(response);
+//     // get the data from response and store it in variable gif
+//     var cityImage = response;
+//       // console.log(gif);
+// // loop through the gif reponse data
+//       for (i = 0; i < cityImage.length; i++){
+//         // create p tag
+//         var p = $("<p>");
+//         //add class of rating to p tags
+//         p.addClass("rating");
+//         // add text to p tag of Rating: and the rating from the gif responses
+//         // p.text("Rating: " + gif[i].rating);
+//         // store img tag in variable gifImg
+//         var cityImg = $("<img>");
+
+//       }
+//   }
+
+
+//     
