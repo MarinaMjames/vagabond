ef807b43e559e03838430a263c72829b72da1342
Jhemarr Anderson <jhemarranderson@Jhemarrs-MacBook-Pro.local>
2017-07-24 23:52:52 -0400

    Database entries complete for LA - DC

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -13,20 +13,20 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
+  app.get("/", (req, res) => {
     res.render("index");
   });
 
-  app.get("/city/lookup", function(req, res){
+  app.get("/city/lookup", (req, res) => {
     res.render("app");
   });
-  app.get("/account", function(req, res){
+  app.get("/account", (req, res) => {
     res.render("profile");
   });
-  app.get("/signup", function(req, res){
+  app.get("/signup", (req, res) => {
     res.render("signup");
   });
-  app.get("/wishlist", function(req, res){
+  app.get("/wishlist", (req, res) => {
     res.render("");
   });
 };
